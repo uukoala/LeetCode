1c8fa7af7ad836bd4100964385c5d43856c68d32
uukoala <uukoala@sina.cn>
2017-02-18 20:51:45 +0800

    9

--- 9.Palindrome_Number.java
+++ 9.Palindrome_Number.java
@@ -1,15 +1,11 @@
 public class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0) return false;
-        int origin = x;
-        long res = 0;
-        while(x != 0){
+        if(x<0 || (x != 0 && x%10==0)) return false; 
+        int res = 0;
+        while(x > res){
             res = res*10 + x%10;
             x /= 10;
         }
-        if(res>Integer.MAX_VALUE || res<Integer.MIN_VALUE || res != origin){
-            return false;
-        }
-        return true;
+        return (res == x || res/10 == x);
     }
 }
