cb49e8d4fad51bbd503aeb6b93fc0e994e77d8af
uukoala <uukoala@sina.cn>
2017-02-18 14:05:04 +0800

    153

---
+++ 153.Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int findMin(int[] nums) {        
+        int begin = 0, end = nums.length-1;
+        while(begin<end){
+            int mid = (begin + end)/2;
+            if(nums[mid] > nums[end]) begin = mid+1;
+            else end = mid;
+        }
+        return nums[begin];
+    }
+}
